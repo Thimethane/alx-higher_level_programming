24765021df72495666b3c70e49c500c7948ba0a4
thimethane <thimethane@gmail.com>
2023-09-28 22:38:30 +0200

    Advanced

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Send a PUT request with a custom header to cause the server to respond with the desired message
-curl -s -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" "http://0.0.0.0:5000/catch_me"
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
